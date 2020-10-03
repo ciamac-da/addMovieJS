30f517ff19f41b5c1e958c0720fa5a6b5644cf2c
ciamac-da <ciamac.davoudi@gmail.com>
2020-10-03 17:28:27 +0200

    Verified by Cia v1.0.7

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,7 +5,8 @@ const startAddMovieButton = document.querySelector('header button');
 // const startAddMovieButton = document.querySelector('header').lastElementChild;
 const backdrop = document.getElementById("backdrop");
 const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive");
-
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling; 
+const userInfos = addMovieModal.querySelectorAll("input");
 
 const toggleBackdrop = () => {
     backdrop.classList.toggle("visible");
@@ -16,14 +17,32 @@ const toggleMovieModal = () =>{
     toggleBackdrop();
 }
 
+const addMovieHandler = () =>{
+const titleValue = userInfos[0].value;
+const imageUrlValue = userInfos[1].value;
+const ratingValue = userInfos[2].value;
+
+if(titleValue.trim() === "" || 
+imageUrlValue.trim() === "" || 
+ratingValue.trim() === ""   ||
++ratingValue < 1            ||
++ratingValue > 10
+){
+alert("Please enter a valid number between 1 and 10!")
+}
+return;
+}
+
+
 const backdropClickHandler = () => {
     toggleMovieModal();
 }
 
-const cancelAddMovie = () =>{
+const cancelAddMovieHandler = () =>{
     toggleMovieModal();
 }
 
 startAddMovieButton.addEventListener("click", toggleMovieModal);
 backdrop.addEventListener("click", backdropClickHandler);
-cancelAddMovieButton.addEventListener("click",cancelAddMovie);
+cancelAddMovieButton.addEventListener("click",cancelAddMovieHandler);
+confirmAddMovieButton.addEventListener("click", addMovieHandler);
